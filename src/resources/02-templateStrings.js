ca34628407635523e53681235b237a0afeb4c2c1
Jorge Quintui <jorgealequinn@gmail.com>
2023-06-09 05:18:23 +0000

    ADD: Template strings

---
+++ src/resources/02-templateStrings.js
@@ -0,0 +1,10 @@
+// Template strings 
+
+const firstName = 'Jorge';
+const lastName = 'Quintui';
+
+//const fullName = firstName + ' ' + lastName;
+
+const fullName = `Hello ${firstName} ${lastName}`;
+
+console.log(fullName);
